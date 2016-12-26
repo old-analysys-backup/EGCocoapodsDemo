17f0665fcc71ba267c3ca1c5c1f871ce8cb31dac
analysys <zhiku@analysys.com.cn>
2016-12-26 14:42:16 +0800

    1.0 commit

---
+++ EmptyProject/Classes/StaticlibDemo.m
@@ -0,0 +1,18 @@
+//
+//  StaticlibDemo.m
+//  StaticlibDemo
+//
+//  Created by analysys on 2016/12/23.
+//  Copyright © 2016年 analysys. All rights reserved.
+//
+
+#import "StaticlibDemo.h"
+
+@implementation StaticlibDemo
+
++ (void)printHello
+{
+    NSLog(@"hello world!");
+}
+
+@end
